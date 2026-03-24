0d7d3a9580d15e566b99210a48d543bbe45ad1fe
atilla baspinar <atillabaspinar@gmail.com>
2026-03-24 13:03:40 +0100

    change db name

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -8,7 +8,7 @@ export default new DataSource({
   port: Number(process.env.DATABASE_PORT ?? 5432),
   username: process.env.DATABASE_USERNAME ?? 'postgres',
   password: process.env.DATABASE_PASSWORD ?? 'postgres',
-  database: process.env.DATABASE_NAME ?? 'chartstudiodb',
+  database: process.env.DATABASE_NAME ?? 'postgres',
   entities: [User],
   migrations: ['src/database/migrations/*{.ts,.js}'],
 });
